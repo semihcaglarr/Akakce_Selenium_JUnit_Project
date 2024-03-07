74e6b7481a71041fc48067b2df0f496a2aba5c34
sammycaglar <caglarsemih1@outlook.com>
2024-03-08 02:05:52 +0300

    Initial commit

---
+++ src/US_104_OturumAcma_Login/TC_0402_OturumAcma_Login.java
@@ -0,0 +1,83 @@
+package US_104_OturumAcma_Login;
+
+import Utility.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class TC_0402_OturumAcma_Login extends BaseDriver {
+    @Test
+    public void LoginTest(){
+        driver.get("https://www.akakce.com/");
+        WebElement girisyap= driver.findElement(By.xpath("//a[text()='Giriş Yap']"));
+        girisyap.click();
+
+        WebElement eposta= driver.findElement(By.cssSelector("input[id='life']"));
+        eposta.sendKeys("SeleniumProjectTeam@gmail.com");
+
+        WebElement sifre= driver.findElement(By.cssSelector("input[id='lifp']"));
+        sifre.sendKeys("Bugfighters1234!");
+
+        WebElement loginbutton=driver.findElement(By.cssSelector("input[id='lfb']"));
+        loginbutton.click();
+
+        WebElement menu= driver.findElement(By.cssSelector("a[id='H_a_v8']"));
+        wait.until(ExpectedConditions.visibilityOf(menu));
+
+        Actions aksiyonDriver=new Actions(driver);
+        Action aksiyon=aksiyonDriver.moveToElement(menu).build();// elementin üzerine gel
+        aksiyon.perform(); // etki olustur(maus üzerinde)
+
+        WebElement hesabim= driver.findElement(By.xpath("//a[text()='Hesabım']"));
+
+        //1.Yöntem
+        //Assert.assertTrue("Giris yapilamadi",hesabim.getText().equals("Hesabım"));
+
+        //2.Yöntem
+        hesabim.click();
+        wait.until(ExpectedConditions.urlToBe("https://www.akakce.com/akakcem/"));
+        Assert.assertTrue("Giris yapilamadi",driver.getCurrentUrl().equals("https://www.akakce.com/akakcem/"));
+
+        BekleKapat();
+    }
+    @Test
+    public void EksikBilgiTest(){
+        driver.get("https://www.akakce.com/");
+        WebElement girisyap= driver.findElement(By.xpath("//a[text()='Giriş Yap']"));
+        girisyap.click();
+
+        WebElement eposta= driver.findElement(By.cssSelector("input[id='life']"));
+        eposta.sendKeys("SeleniumProject@gmail.com");
+
+        WebElement sifre= driver.findElement(By.cssSelector("input[id='lifp']"));
+        sifre.sendKeys("1234");
+
+        WebElement loginbutton=driver.findElement(By.cssSelector("input[id='lfb']"));
+        loginbutton.click();
+
+        WebElement hata= driver.findElement(By.xpath("//p[text()='Böyle bir kullanıcı yok.']"));
+
+        wait.until(ExpectedConditions.visibilityOf(hata));
+        Assert.assertTrue("Hatali sayfa girisi",hata.getText().equals("Böyle bir kullanıcı yok."));
+        BekleKapat();
+    }
+    @Test
+    public void NullDegerTest(){
+        driver.get("https://www.akakce.com/");
+        WebElement girisyap= driver.findElement(By.xpath("//a[text()='Giriş Yap']"));
+        girisyap.click();
+
+        WebElement loginbutton=driver.findElement(By.cssSelector("input[id='lfb']"));
+        loginbutton.click();
+
+        WebElement hatabutton= driver.findElement(By.xpath("//b[text()='Tamam']"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(hatabutton));
+        Assert.assertTrue("Hatali sayfa yönlendirme",hatabutton.isDisplayed());
+        BekleKapat();
+    }
+}
---
+++ src/US_107_HesapSilme/TC_0701_HesapSilme.java
@@ -0,0 +1,32 @@
+package US_107_HesapSilme;
+
+import Utility.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class TC_0701_HesapSilme extends BaseDriver {
+    @Test
+    public void TC_0701() {
+        // Nagigate to website
+        driver.get("https://www.akakce.com/");
+        // Navigate to the login page
+        driver.findElement(By.xpath("//a[text()='Giriş Yap']")).click();
+        // Enter the credentials and click the "Giriş Yap" button
+        driver.findElement(By.cssSelector("input[id='life']")).sendKeys("SeleniumProjectTeam@gmail.com");
+        driver.findElement(By.cssSelector("input[id='lifp']")).sendKeys("Bugfighters1234!");
+        driver.findElement(By.xpath("//input[@id='lfb']")).click();
+        // Navigate to the user profile and click the "Hesabımı Sil" button
+        driver.findElement(By.linkText("Selen")).click();
+        driver.findElement(By.linkText("Hesabımı Sil")).click();
+        // Enter the password for account deletion confirmation
+        driver.findElement(By.cssSelector("input[id='p']")).sendKeys("Bugfighters1234!" + Keys.ENTER);
+        // Find the confirmation message and check if the account deletion is successful
+        WebElement condition = driver.findElement(By.cssSelector("div[id='C4w'] i"));
+        Assert.assertEquals("Hesap silinemedi!", "Hesabın silindi", condition.getText().substring(0, 15));
+
+        BekleKapat();
+    }
+}
